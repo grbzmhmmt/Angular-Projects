400d5ab0e86928e25d37bbbd3f361a3bcf4fd804
grbz <grbzmhmmt@gmail.com>
2019-01-18 00:27:49 +0300

    Added quintessential content
    Added quintessential content

--- src/app/quintessentials/quintessential/quintessential.component.ts
+++ src/app/quintessentials/quintessential/quintessential.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { QuintessentialWords } from './quintessential';
+import { QuintessentialWordsList } from './quintessential-list.mock';
 
 @Component({
   selector: 'app-quintessential',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quintessential.component.css']
 })
 export class QuintessentialComponent implements OnInit {
-
+  quintessentials:QuintessentialWords[];
+  quintessential:QuintessentialWords;
+  rd:number;
   constructor() { }
 
   ngOnInit() {
+    this.quintessentials=QuintessentialWordsList;
+    this.quintessential=this.quintessentials.pop();
+  }
+
+  @HostListener('document:click', ['$event'])
+  public documentClick(event: Event): void {
+    this.rd=Math.floor(Math.random() * (this.quintessentials.length)) + 0;
+    this.quintessential=this.quintessentials[this.rd];
   }
 
 }
---
+++ src/app/quintessentials/quintessential/quintessential.ts
@@ -0,0 +1,4 @@
+export class QuintessentialWords{
+    author:string;
+    word:string;
+}
